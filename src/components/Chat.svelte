0e116101b1c4c45db16b7612f9b4bbae1b6e20aa
Ayoub Coding <ayoubprogramming95@gmail.com>
2023-08-23 22:30:37 +0100

    Update Chat.svelte

--- src/components/Chat.svelte
+++ src/components/Chat.svelte
@@ -78,3 +78,86 @@
   <input placeholder="Message" type="text" bind:value={newMessage} />
   <button type="submit">Send</button>
 </form>
+
+<style>
+  /* Montserrat Font */
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap');
+  
+  /* Global Styles */
+  :global(body) {
+    font-family: 'Montserrat', sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #000000;
+    color: #fff;
+  }
+
+  /* Component Styles */
+  .messages {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-top: 20px;
+  }
+
+  .msg {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    background-color: #110;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    transition: background-color 0.3s ease-in-out;
+  }
+
+  .msg:hover {
+      background-color: #FF5F1F;
+      color: #000;
+  }
+
+  .avatar {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+
+  .msg-text {
+    margin-top: 5px;
+    font-size: 16px;
+  }
+
+  /* HTML element styles */
+  form {
+    display: flex;
+    align-items: center;
+    margin-top: 20px;
+  }
+
+  input[type="text"] {
+    flex: 1;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    color: #fff;
+    background-color: #000;
+    font-family: 'Montserrat';
+  }
+
+  button[type="submit"] {
+    padding: 10px 20px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease-in-out;
+    font-family: 'Montserrat';
+  }
+
+  button[type="submit"]:hover {
+    background-color: #0056b3;
+  }
+</style>

1fdcc358453ef925bbcc10650cf18da8fd91c212
Ayoub Coding <ayoubprogramming95@gmail.com>
2023-08-24 11:41:55 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
     import Register from "./components/Register.svelte";
     import Chat from "./components/Chat.svelte";
     import { currentUser } from "./lib/pocketbase-config";
+    import { scrollTo } from 'svelte';
 </script>
 
 <body>
@@ -9,7 +10,7 @@
     <Register />
 
     {#if $currentUser}
-      <Chat />
+      <Chat on:messageAdded={() => scrollTo(document.body, document.body.scrollHeight)}/>
     {/if}
 </body>
 
